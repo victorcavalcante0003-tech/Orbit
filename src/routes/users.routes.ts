ad0c7c41f3b448de45d2ee4aa4b5b80117fe6b6d
Victor <victorcavalcante0003@gmail.com>
2026-07-09 19:05:40 -0300

    Endpoints Finalizados

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 import { authMiddleware } from '../middlewares/auth.middleware';
 import { getMe, updateMe, getUserById, getUserPosts } from '../controllers/users.controller';
+import { followUser, unfollowUser, listFollowers, listFollowing } from '../controllers/follow.controller';
 
 const router = Router();
 
 router.get('/me', authMiddleware, getMe);
 router.patch('/me', authMiddleware, updateMe);
+
+router.post('/:id/follow', authMiddleware, followUser);
+router.delete('/:id/follow', authMiddleware, unfollowUser);
+router.get('/:id/followers', listFollowers);
+router.get('/:id/following', listFollowing);
+
 router.get('/:id', getUserById);
 router.get('/:id/posts', getUserPosts);
 
